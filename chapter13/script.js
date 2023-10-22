c04b68e0228d4b8c118c7c088035e1ad2d50a289
seoyeoning <minalunpark@naver.com>
2023-10-22 23:49:44 +0900

    chapter10~13

---
+++ chapter13/script.js
@@ -0,0 +1,130 @@
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
+
+let correctQuestionNUm;
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  correctQuestionNUm = 0;
+  console.log('Started');
+  startButton.classList.add('hide');
+  shuffledQuestions = question.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  if (correct) {
+    correctQuestionNUm++;
+  }
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+  } else {
+    console.log(correctQuestionNUm);
+    startButton.innerText =
+      'Your Score: ' + correctQuestionNUm + ' / ' + question.length;
+    startButton.classList.remove('hide');
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+const question = [
+  {
+    question: 'what is 2 + 2',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false },
+    ],
+  },
+  {
+    question: 'what does HTML stand for?',
+    answers: [
+      { text: 'Hyperlinks and Text Markup Language', correct: false },
+      { text: 'Hyper Text Markup Language', correct: true },
+      { text: 'Home Tool Markup Language', correct: false },
+    ],
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'Yes!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false },
+    ],
+  },
+  {
+    question: 'Who is making the web standards?',
+    answers: [
+      { text: 'Mozilla', correct: false },
+      { text: 'The World Wide Web Consortium', correct: true },
+      { text: 'Microsoft', correct: false },
+      { text: 'Google', correct: false },
+    ],
+  },
+  {
+    question: 'What does CSS stand for?',
+    answers: [
+      { text: 'Cascading Style Sheets', correct: true },
+      { text: 'Colorful Style Sheets', correct: false },
+      { text: 'Creative Style Sheets', correct: false },
+      { text: 'Computer Style Sheets', correct: false },
+    ],
+  },
+];
